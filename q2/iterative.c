0a1b580b33e83a7fdf3c3cbb32ecdf11a926d0bb
Andy Pi <p660011@gmail.com>
2015-10-17 18:07:24 +0800

    q2

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,29 @@
 /* FIXME: Implement! */
 
-int main()
-{
-    return 0;
+char smallest_character(char str[], char c){
+    char firstStr = *str;
+    
+    while(*str!='\0')
+     {
+         if(*str>c)
+           return *str;
+      str++;
+      }
+    return firstStr;
 }
+
+
+int main()
+    {
+        char test[]="cfjpv";
+        char test2[] ="cck";
+
+        printf("%c\n",smallest_character(test,'a'));
+        printf("%c\n",smallest_character(test,'c'));
+        printf("%c\n",smallest_character(test,'z'));
+        printf("%c\n",smallest_character(test2,'f'));
+
+
+        return 0;
+    }
+
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,25 @@
 /* FIXME: Implement! */
+char smallest_character(char str[],char c)
+{
+    if(*str=='\0')
+        return *str;
+    else if(*str>c){
+        return *str;
+    }else{
+        str++;
+        return smallest_character(str,c);
+    }
+}
+
+
 
 int main()
-{
-    return 0;
-}
+    {
+        char test[] = "cfjpv";
+        char test2[] = "cck";
+        printf("%c\n",smallest_character(test,'a'));
+        printf("%c\n",smallest_character(test,'c'));
+        printf("%c\n",smallest_character(test,'z'));
+        printf("%c\n",smallest_character(test2,'f'));
+        return 0;
+    }
